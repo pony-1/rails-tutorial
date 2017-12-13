c4c2a6f6a335e5d25341a7cfc38e6831ac590380
pony <111l000393@gmail.com>
2017-12-13 16:15:06 +0900

    11

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,13 +9,18 @@ def remember(user)
     cookies.permanent[:remember_token] = user.remember_token
 end
 
+# 渡されたユーザーがログイン済みユーザーであればtrueを返す
+ def current_user?(user)
+   user == current_user
+ end
+
 # 記憶トークンcookieに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -38,5 +43,14 @@ def log_out
   @current_user = nil
 end
 
+# 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
 
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
